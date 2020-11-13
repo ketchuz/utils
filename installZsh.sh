9e0ef0ca96c324550ba47eef5436c18d8c589ad7
Jesus Esparza <jesus.esparza.c4@gmail.com>
2020-11-12 21:07:00 -0600

    Make default shell

--- installZsh.sh
+++ installZsh.sh
@@ -6,6 +6,7 @@ installZsh() {
     sudo apt-get install zsh -y
     sudo apt-get install git-core -y
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null
+    chsh -s `which zsh`
 }
 
 installZsh
