f1baf963408e1fe97a5e5afed0b3bfe8b2181c88
Jesus Esparza <jesus.esparza.c4@gmail.com>
2020-11-12 20:22:46 -0600

    Test scripts

---
+++ gitlabRunnerSetup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+set -eo pipefail
+export BASH_UTILS_LOCATION="/root/bash-utils"
+
+
+ensureBashUtilsAreInstalled() {
+    if [ ! -d "${BASH_UTILS_LOCATION}" ]; then
+        local utilsRepo=https://github.com/stefanpl/bash-utils
+        # local utilsRepo=/home/stefan/webdev/bash-utils
+        echo "Cloning bash utils …"
+        git clone --quiet ${utilsRepo} ${BASH_UTILS_LOCATION} > /dev/null
+        echo "bash utils cloned!"
+    fi
+}
+
+installZsh() {
+    logInfo "Installing Oh-my-ZSH..."
+    sudo apt-get update -y
+    sudo apt-get install zsh -y
+    sudo apt-get install git-core -y
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >/dev/null
+    logSuccess "Oh-my-ZSH installed"
+}
+
+installGitlabRunner() {
+    logInfo "Installing Gitlab Runner..."
+    getent passwd gitlab-runner > /dev/null 2&>1
+
+    if [ $? -eq 0 ]; then
+        logInfo "Gitlab Runner already exists"
+    else
+        sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
+        sudo chmod +x /usr/local/bin/gitlab-runner
+
+        sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+        sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
+        sudo gitlab-runner start
+        sudo gitlab-runner register
+        logSuccess "Gitlab runner installed and registered"
+    fi
+}
+
+installDocker() {
+    logInfo "Installing Docker"
+    sudo apt install docker.io -y
+    sudo echo "gitlab-runner ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+    sudo usermod -aG docker gitlab-runner
+    sudo gitlab-runner restart
+    logSuccess "Docker installed"
+}
+
+installDockerCompose() {
+    logInfo "Installing docker-compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    logSuccess "Docker Compose installed"
+}
+ensureBashUtilsAreInstalled
+
+source ${BASH_UTILS_LOCATION}/bootstrap.sh
+
+
+installGitlabRunner
+installDocker
+installDockerCompose
+installZsh
+
+logSuccess "Server setup done 🚀 Ready to do some deploys"
