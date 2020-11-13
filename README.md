ea8ac2fd5527bb1cdd47a9a97fe1d234d2657917
Jesus Esparza <jesus.esparza.c4@gmail.com>
2020-11-12 20:49:30 -0600

    READme updated

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# utils
+# Utilities
+
 Scripts and more
+
+## Add Swap
+
+```bash
+sh -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/ketchuz/utils/main/addSwap.sh)"
+```
+
+## Gitlab Runner Server Setup
+
+```bash
+sh -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/ketchuz/utils/main/gitlabRunnerSetup.sh)"
+```
--- gitlabRunnerSetup.sh
+++ gitlabRunnerSetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eo pipefail
+
 export BASH_UTILS_LOCATION="/root/bash-utils"
 
 
@@ -25,13 +25,13 @@ installZsh() {
 installGitlabRunner() {
     logInfo "Installing Gitlab Runner..."
     getent passwd gitlab-runner > /dev/null 2&>1
-
+    
     if [ $? -eq 0 ]; then
         logInfo "Gitlab Runner already exists"
     else
         sudo curl -L --output /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
         sudo chmod +x /usr/local/bin/gitlab-runner
-
+        
         sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
         sudo gitlab-runner install --user=gitlab-runner --working-directory=/home/gitlab-runner
         sudo gitlab-runner start
